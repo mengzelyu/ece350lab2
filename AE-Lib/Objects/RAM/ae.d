85a875715a613749a6f43628a91165fd5bf2deb1
mengzelyu <51139549+mengzelyu@users.noreply.github.com>
2021-07-12 22:32:50 -0400

    Add files via upload

---
+++ AE-Lib/Objects/RAM/ae.d
@@ -0,0 +1,12 @@
+.\objects\ram\ae.o: src\ae\ae.c
+.\objects\ram\ae.o: ..\include\ae.h
+.\objects\ram\ae.o: ..\include\bsp\LPC1768\uart_polling.h
+.\objects\ram\ae.o: D:\Mengze's stuff\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\ram\ae.o: ..\include\bsp\LPC1768\uart_def.h
+.\objects\ram\ae.o: ..\include\printf.h
+.\objects\ram\ae.o: D:\Mengze's stuff\ARM\ARMCC\Bin\..\include\stdarg.h
+.\objects\ram\ae.o: ..\include\rtx.h
+.\objects\ram\ae.o: ..\include\rtx_ext.h
+.\objects\ram\ae.o: ..\include\common.h
+.\objects\ram\ae.o: ..\include\common_ext.h
+.\objects\ram\ae.o: ..\include\bsp\LPC1768\lpc1768_mem.h
---
+++ AE-Lib/Objects/RAM/ae_tasks.d
@@ -0,0 +1,12 @@
+.\objects\ram\ae_tasks.o: src\ae\ae_tasks.c
+.\objects\ram\ae_tasks.o: src\ae\ae_tasks.h
+.\objects\ram\ae_tasks.o: ..\include\rtx.h
+.\objects\ram\ae_tasks.o: ..\include\rtx_ext.h
+.\objects\ram\ae_tasks.o: ..\include\common.h
+.\objects\ram\ae_tasks.o: ..\include\common_ext.h
+.\objects\ram\ae_tasks.o: ..\include\bsp\LPC1768\lpc1768_mem.h
+.\objects\ram\ae_tasks.o: ..\include\bsp\LPC1768\uart_polling.h
+.\objects\ram\ae_tasks.o: D:\Mengze's stuff\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\ram\ae_tasks.o: ..\include\bsp\LPC1768\uart_def.h
+.\objects\ram\ae_tasks.o: ..\include\printf.h
+.\objects\ram\ae_tasks.o: D:\Mengze's stuff\ARM\ARMCC\Bin\..\include\stdarg.h
